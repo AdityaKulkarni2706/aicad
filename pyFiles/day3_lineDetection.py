ce3790347f449c9a3757f029d4fb080d5f9bb2a7
Aditya <noxx.music27@gmail.com>
2026-01-05 16:33:33 +0300

    lineDetection added

---
+++ pyFiles/day3_lineDetection.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import display
+
+img = cv2.imread("../images/sudoku.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 50, 150)
+
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, threshold=75, minLineLength=75, maxLineGap=10)
+
+if lines is not None:
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+display.displayImg(img, "hough lines")
